2699b85fa17061f530d6b9cde9d66179fbc81023
Taylor <lxmachina@gmail.com>
2016-05-18 00:03:48 -0400

    Add my tests

---
+++ cs540p3_foxhall_taylor/Interpolate.cpp
@@ -0,0 +1,50 @@
+#include "Interpolate.hpp"
+
+#include <sstream>
+
+using namespace cs540;
+
+int main() {
+  // Test argument lengths
+  std::cout << Interpolate("Test ") << std::endl;
+  std::cout << Interpolate("Test %", 32) << std::endl;
+  std::cout << Interpolate("Test % happy %", 32, "one MILLION dollars") << std::endl;
+  std::cout << Interpolate("Test % happy % GabeN %", 32, "one MILLION dollars", 2.9) << std::endl;
+  std::cout << Interpolate("Test %%%", 'm', 'e', "me") << std::endl;
+
+  // Test mismatched arguments
+  try {
+    std::cout << Interpolate("Test ", 56) << std::endl;
+  } catch (WrongNumberOfArgs e) {
+    std::cout << e.what() << std::endl;
+  }
+  try {
+    std::cout << Interpolate("Test %") << std::endl;
+  } catch (WrongNumberOfArgs e) {
+    std::cout << e.what() << std::endl;
+  }
+  try {
+    std::cout << Interpolate("Test %%%", 'm', 'e', "me", 33) << std::endl;
+  } catch (WrongNumberOfArgs e) {
+    std::cout << e.what() << std::endl;
+  }
+
+  // Test backslash
+  std::cout << Interpolate(R"(I have 300\% more confidence in \dank memes due to the recent 500\% decrease in rare pepes)") << std::endl;
+  auto a = Interpolate(R"(Haskell is for \%NOOBS\%, dirty % pureists)", "pleb");
+  std::stringstream ss;
+  ss << a;
+  std::cout << ss.str() << std::endl;
+  assert(ss.str() == "Haskell is for %NOOBS%, dirty pleb pureists");
+
+  // ASSAULT
+  std::cout << Interpolate(R"(%%\%\\%%%\%\%\%%%)", 0, 1, 2, 3, 4, 5) << std::endl;
+
+  // Windows environment variables
+  std::cout << Interpolate(R"(\%%\%;\%%\%;)", "HOME", "SYSTEM") << std::endl;
+
+  // Test manip
+  std::cout << Interpolate("Twenty is equal to %, and equal to %! %", 20, std::hex, 0x14, "GREAT!");
+
+  return 0;
+}
